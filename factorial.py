be1f9bcfd626fcb07ce1c71f853c6b676fc0f7d9
shaunmelkam-hue <shaunmelkam@gmail.com>
2026-08-16 17:52:46 -0500

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial(x):
+    """Return the factorial of a non-negative integer x."""
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return x * factorial(x - 1)
+
+print(factorial(0))
+print(factorial(1))
+print(factorial(2))
+print(factorial(5))
+print(factorial(10))
