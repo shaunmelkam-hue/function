e06222000dcf24ddff46b2c924ce058e2c364d3d
shaunmelkam-hue <shaunmelkam@gmail.com>
2026-08-22 17:35:31 -0500

    Add files via upload

---
+++ activity1_break.py
@@ -0,0 +1,8 @@
+a = input("Enter a word: ")
+
+for i in a:
+    if i == "A":
+        print("The letter 'a' is present in the word.")
+        break
+    else:
+        print("The letter 'a' is not present in the word.")
